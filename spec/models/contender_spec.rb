ff696027e8b197f72e9c7ad66905b07cf321acce
Cyril Fluck <cfluck@Cyrils-MacBook-Pro.local>
2011-11-12 21:38:04 -0800

    first tests for contender and match

--- spec/models/contender_spec.rb
+++ spec/models/contender_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Contender do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    before(:all) do
+      @contender = Contender.new
+    end
+
+    [:set1, :set2, :set3].each do |set|
+      it "ensures #{set} is an integer greater or equal to 0" do
+        # @contender.should have_errors_on(set)
+      end
+    end
+  end
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe Match do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'initialization' do
+    context 'with no contenders' do
+      it 'creates 2 contenders' do
+        match = Match.new
+        match.contenders.size.should == 2
+        match.contenders[0].side.should == 0
+        match.contenders[1].side.should == 1
+      end
+    end
+
+    context 'with contenders' do
+      before(:each) do
+        @match_ref = Match.new(:date => Date.today)
+        user = User.create(:email => 'john@doe.com', :password => 'tototo')
+        @match_ref.contenders.each do |contender|
+          contender.update_attributes(:user_id => user.id,
+                                      :set1 => 0,
+                                      :set2 => 0,
+                                      :set3 => 0)
+        end
+        @match_ref.save
+      end
+
+      it 'doesnt create new contenders' do
+        match = Match.find(@match_ref.id)
+        match.contenders.size.should == 2
+        match.contenders.each do |contender|
+          contender.persisted?.should be_true
+        end
+      end
+    end
+  end
 end
