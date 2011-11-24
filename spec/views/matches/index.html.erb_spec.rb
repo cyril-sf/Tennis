bf4ab7f671d9bd3140fd197087794adaed07acaa
Cyril Fluck <cfluck@Cyrils-MacBook-Pro.local>
2011-11-24 08:29:13 -0800

    fix test

--- spec/views/matches/index.html.erb_spec.rb
+++ spec/views/matches/index.html.erb_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 describe "matches/index.html.erb" do
   before(:each) do
-    assign(:matches, [
-      stub_model(Match),
-      stub_model(Match)
-    ])
+    user = User.create :email => "email"
+
+    1.upto(2) do |index|
+      m = Match.create
+      m.contenders.each do |contender|
+        contender.update_attributes(  :set1 => 1,
+                                      :set2 => 2,
+                                      :set3 => 3 )
+      end
+    end
+    assign(:matches, Match.all)
   end
 
   it "renders a list of matches" do
