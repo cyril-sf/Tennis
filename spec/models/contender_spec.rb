0146d3dc5abbc22686a130a2e4d14f862f372fe9
Matt McGinnis <matt@mcgintech.com>
2011-12-09 00:44:26 -0800

    Add association and attribute unit tests

--- spec/models/contender_spec.rb
+++ spec/models/contender_spec.rb
@@ -1,6 +1,24 @@
 require 'spec_helper'
 
 describe Contender do
+  describe 'attributes' do
+    [:side, :set1, :set2, :set3].each do |attr|
+      it "responds to #{attr}" do
+        Contender.new.should respond_to(attr)
+      end
+    end
+  end
+
+  describe 'associations' do
+    it 'belongs to a user' do
+      Contender.should belong_to :user
+    end
+
+    it 'belongs to a match' do
+      Contender.should belong_to :match
+    end
+  end
+
   describe 'validation' do
     before(:all) do
       @contender = Contender.new
