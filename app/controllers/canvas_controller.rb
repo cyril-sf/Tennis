628ddd4245b48a9ab9c492d1bcc43ff32806f0bf
Cyril Fluck <cyril@fluck.fr>
2011-12-08 21:24:07 -0800

    sign in a user only if we get a facebook id

--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -1,6 +1,6 @@
 class CanvasController < ApplicationController
   def index
-    if @signed_request
+    if @signed_request && @signed_request["user_id"]
       user = User.where(:facebook_id => @signed_request["user_id"]).first
       sign_in( user ) if user
     end
